7c0e26c2eb3100300e1754efa0acf49708fea463
Simone <simone.dangelo636@gmail.com>
2023-12-08 09:09:01 +0100

    sidebar per utenti diversi

--- src/componets/SideBar.jsx
+++ src/componets/SideBar.jsx
@@ -1,36 +1,72 @@
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const SideBar = () => {
+  const user = useSelector(state => state.login.user);
+  const vehicle = useSelector(state => state.vehicles.vehicle);
   const hadlerSubmit = e => {
     e.preventDefault();
   };
   return (
     <>
       <div className="SideBar">
-        <Container>
-          <Row
-            className="d-flex flex-row"
-            style={{
-              height: "100vh"
-            }}>
-            <Col className="border">
-              <Row className="mt-3 d-flex flex-column g-4">
-                <Col>
-                  <Link to="/register_vehicle">Iscrivi il tuo veicolo</Link>
-                </Col>
-                <Col>
-                  <Link to="/profile_vehicle">il tuo veicolo</Link>
-                </Col>
-                <Col>
-                  <Link to="/profile_owner">Profile</Link>
-                </Col>
-                <Col>
-                  <Link to="/log_out">Logout</Link>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+        {user.role === "CUSTOMER" ? (
+          <Container>
+            <Row
+              className="d-flex flex-row"
+              style={{
+                height: "100vh"
+              }}>
+              <Col className="border">
+                <Row className="mt-3 d-flex flex-column g-4">
+                  <Col>
+                    <Link to="/register_vehicle">Prenota un van</Link>
+                  </Col>
+                  <Col>
+                    <Link to="/profile_vehicle">Le tue prenotazioni</Link>
+                  </Col>
+                  <Col>
+                    <Link to="/profile_customer">Profile</Link>
+                  </Col>
+                  <Col>
+                    <Link to="/log_out">Logout</Link>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        ) : user.role === "OWNER" ? (
+          <Container>
+            <Row
+              className="d-flex flex-row"
+              style={{
+                height: "100vh"
+              }}>
+              <Col className="border">
+                <Row className="mt-3 d-flex flex-column g-4">
+                  {vehicle ? (
+                    <Col>
+                      <Link to="/profile_vehicle">il tuo veicolo</Link>
+                    </Col>
+                  ) : (
+                    <Col>
+                      <Link to="/register_vehicle">Iscrivi il tuo veicolo</Link>
+                    </Col>
+                  )}
+
+                  <Col>
+                    <Link to="/profile_owner">Profile</Link>
+                  </Col>
+                  <Col>
+                    <Link to="/log_out">Logout</Link>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
